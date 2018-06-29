e9fd23fe99514ebfcc6a952b0470cf5a8d583d76
Tammy Tian <tiantammy@outlook.com>
2018-06-29 20:20:14 +0200

    added client origin

--- config.js
+++ config.js
@@ -2,7 +2,7 @@
 
 module.exports = {
   PORT: process.env.PORT || 8080,
-  CLIENT_ORIGIN: process.env.CLIENT_ORIGIN || 'http://localhost:3000',
+  CLIENT_ORIGIN: process.env.CLIENT_ORIGIN || 'https://hiram-tammy-petful-client.github.io',
   DATABASE_URL:
         process.env.DATABASE_URL || 'mongodb://localhost/thinkful-backend',
   TEST_DATABASE_URL:
